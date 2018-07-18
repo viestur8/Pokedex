1aa4dd574a4faa1dabd1e9071af9d5a4bcb0ffdd
viestur8 <twotigers@vivaldi.net>
2018-07-18 16:03:39 -0400

    inital commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let apiUrl = "https://pokeapi.co/api/v2/pokemon/";
+let input = document.querySelector(".pokemon-input");
+let pokemonName = document.querySelector(".pokemon-name");
+let pokemonImage = document.querySelector(".pokemon-image");
+if (pokemonImage && pokemonImage.style) {
+  pokemonImage.style.height = '400px';
+  pokemonImage.style.width = '600px';
+}
+
+function getPokemon() {
+  axios.get(apiUrl + input.value)
+    .then(function(response) {
+      pokemonName.innerHTML = response.data.forms[0].name;
+      pokemonImage.src = response.data.sprites.front_default;
+    })
+    .catch(function(error) {
+      pokemonName.innerHTML = "(An error has occurred.)";
+      pokemonImage.src = "";
+    });
+}
+
+let button = document.querySelector(".pokemon-button");
+button.addEventListener("click", getPokemon);
